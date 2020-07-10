30f5a0dfde0e8115878048ba8faaa400e15dd6e1
Jabin Choi <jchoi110@my.centennialcollege.ca>
2020-07-10 14:35:35 -0400

    Add Contact Details section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
+import ContactDetails from './ContactDetails';
 
 export default class Contact extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {
+      selectedKey: -1,
       keyword: '',
       contactData: [
         {
@@ -27,12 +29,21 @@ export default class Contact extends React.Component {
       ]
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange(e) {
     this.setState({
       keyword: e.target.value
     })
+  }
+
+  handleClick(key) {
+    this.setState({
+      selectedKey: key
+    });
+
+    console.log(key, 'is selected');
   }
 
   render(){
@@ -48,7 +59,11 @@ export default class Contact extends React.Component {
         return contact.name.toLowerCase().indexOf(this.state.keyword.toLowerCase()) > -1;
       })
       return data.map((contact, i) => {
-        return (<ContactInfo contact={contact} key={i} />)
+        return (<ContactInfo
+          contact={contact}
+          key={i}
+          onClick={() => {this.handleClick(i)}}
+          />)
       })
     }
 
@@ -62,6 +77,10 @@ export default class Contact extends React.Component {
           onChange={this.handleChange}
         />
         <div>{mapToComponents(this.state.contactData)}</div>
+        <ContactDetails
+          isSelected={this.state.selectedKey != -1}
+          contact={this.state.contactData[this.state.selectedKey]} 
+        />
       </div>
     );
   }
---
+++ src/components/ContactDetails.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class ContactDetails extends React.Component {
+  render(){
+
+    const details = (
+      <div>
+        <p>{this.props.contact.name} <br/>
+        {this.props.contact.phone}</p>
+      </div>
+    );
+    const blank = (<div>Not selected</div>);
+
+    return(
+      <div>
+        <h1>Details</h1>
+        <p>NOTE: To see details, click the name.</p>
+        {this.props.isSelected ? details : blank}
+      </div>
+    );
+  }
+}
+
+ContactDetails.defaultProps = {
+  contact: {
+    name: '',
+    phone: ''
+  }
+};
